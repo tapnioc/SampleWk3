23f01f159927cf3a7e45bbd4af8182660a6e1a64
tapnioc <59933584+tapnioc@users.noreply.github.com>
2020-01-22 13:44:45 -0500

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,11 +1,13 @@
 package sample;
 
 //date: 22-01-20
+
 public class Main {
 
     public static void main(String[] args) {
         
 		System.out.println("Learning pull operaations");
+	    System.out.println("learning fetch operations");
 
     }
 
